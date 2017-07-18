84911ab66a227921416a5e7ee53e62b27efdd175
DarushDev <darushfaqiri@gmail.com>
2017-07-18 18:04:45 +0800

    First commit

---
+++ app/src/main/java/com/example/myandroidcardview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.myandroidcardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
